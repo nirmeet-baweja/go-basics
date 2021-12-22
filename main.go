fb81b122bd974bb7d79fbb8d4e60897a6b0d1aee
Nirmeet <nirmeet.baweja@gmail.com>
2021-12-22 12:15:28 +0000

    variables

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 
 // every go file needs to be a part of a package.
 // this main.go file is a part of a package called "main"
-// package main tells the go compiler to compile the file into a standalong executable file at the end
+// package main tells the go compiler to compile the file into a standalone executable file at the end
 package main
 
 // importing a single package
@@ -14,6 +14,8 @@ import "fmt"
 func main() {
 
 	/***************************************************************************/
+	/* Beginning */
+	/***************************************************************************/
 	// function names starts with capitals when they are exported.
 	// Thus when we will use functions from other packages,
 	// the function names will start with capitals.
@@ -21,4 +23,52 @@ func main() {
 	fmt.Println("Hello, ninjas!")
 
 	/***************************************************************************/
+	/* Variables */
+
+	/* go is strongly typed language,
+	 * and variable data type cannot be changed after declaration
+	 */
+	/***************************************************************************/
+
+	/* string variables */
+	var nameOne string = "mario"
+	// when data type is not specified, go infers it from the assigned value
+	var nameTwo = "luigi"
+	// when a variable is declared but not initialised, data type needs to specified
+	var nameThree string
+
+	fmt.Print(nameOne, nameTwo, nameThree)
+
+	// variables can be reassigned new values
+	nameOne = "peach"
+	nameThree = "bowser"
+
+	fmt.Print(nameOne, nameTwo, nameThree)
+
+	// the following is allowed inside functions only
+	// A variable declared as below can be used only inside the function it is declared in
+	nameFour := "yoshi"
+	fmt.Print(nameFour)
+
+	/* int variables */
+	var ageOne int = 20
+	var ageTwo = 30 // type inferred directly from the assigned value
+	ageThree := 40  // short-hand
+
+	fmt.Print(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	var numOne int8 = 25
+	var numTwo int8 = 128   // too large a number for 8-bit
+	var numThree uint = -25 // unsigned ints cannot be negative
+
+	fmt.Print(numOne, numTwo, numThree)
+
+	/* float variables */
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 1965385877.5
+	var scoreThree = 1.5 // inferred as float64
+
+	fmt.Print(scoreOne, scoreTwo, scoreThree)
+
 }
